1046e493ef2d2938a7ad96d817fdb1849a64aebe
cyberhat-stack <mpinnoy1@nait.ca>
2026-08-15 23:21:14 -0600

    cards stylized

--- cardcounter/inventory/forms.py
+++ cardcounter/inventory/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from .models import CardEntry, Rarity
 
+INPUT_CLASSES = "w-full border border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+
 class CardEntryForm(forms.ModelForm):
     rarity = forms.ChoiceField(
-        choices=[(r.value, r.label) for r in Rarity.priced_tiers()]
+        choices=[(r.value, r.label) for r in Rarity.priced_tiers()],
+        widget=forms.Select(attrs={"class": INPUT_CLASSES}),
     )
     
     class Meta:
         model = CardEntry
         fields = ["rarity", "name", "set_name", "quantity", "estimated_value"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "set_name": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "quantity": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "estimated_value": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+        }
